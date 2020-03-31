5532f18c1d42baf75cb8f966f92071d13477ed80
Rares Matei <matei.rar@gmail.com>
2020-03-31 13:30:26 +0100

    update

--- check-ignore.sh
+++ check-ignore.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-if [[ $PROJECT_NAME == 'frontend-react' ]]; then echo "hello"; else echo "goodbye"; fi∂
+if [[ $PROJECT_NAME == 'frontend-react' ]]; then echo "hello"; else echo "goodbye"; fi

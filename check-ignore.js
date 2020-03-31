b0d629d6f8bfb32199b29cd6464c52baacaf71bb
Rares Matei <matei.rar@gmail.com>
2020-03-31 18:07:16 +0100

    update

--- check-ignore.js
+++ check-ignore.js
@@ -1,27 +1,22 @@
-// const exec = require('child_process').exec;
-// const getAffected = `yarn --silent exec nx print-affected --base=${process.env.CACHED_COMMIT_REF} --head=HEAD`;
+const exec = require('child_process').exec;
+const getAffected = `yarn --silent exec nx print-affected --base=${process.env.CACHED_COMMIT_REF} --head=HEAD`;
 
-// process.exit(0);
+exec(getAffected, function(error, stdout, stderr) {
+  process.stdout.write('hey there!');
 
-console.log('hello...');
-process.exit(1);
-
-// exec(getAffected, function(error, stdout, stderr) {
-//   process.stdout.write('hey there!');
-
-//   if (error || stderr) {
-//     process.stdout.write(error);
-//     process.stdout.write(stderr);
-//   }
-//   const changedProjects = JSON.parse(stdout).projects;
-//   const currentProject = process.env.PROJECT_NAME;
-//   console.log('----CACHING-----');
-//   console.log(changedProjects);
-//   console.log(currentProject);
-//   console.log(process.env.CACHED_COMMIT_REF);
-//   if (changedProjects.find(proj => proj === currentProject)) {
-//     process.exit(1);
-//   } else {
-//     process.exit(0);
-//   }
-// });
+  if (error || stderr) {
+    process.stdout.write(error);
+    process.stdout.write(stderr);
+  }
+  const changedProjects = JSON.parse(stdout).projects;
+  const currentProject = process.env.PROJECT_NAME;
+  console.log('----CACHING-----');
+  console.log(changedProjects);
+  console.log(currentProject);
+  console.log(process.env.CACHED_COMMIT_REF);
+  if (changedProjects.find(proj => proj === currentProject)) {
+    process.exit(1);
+  } else {
+    process.exit(0);
+  }
+});

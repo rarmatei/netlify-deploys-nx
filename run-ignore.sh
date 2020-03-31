ef2a9c811ac408d1e246509bcb0227f0374c9733
Rares Matei <matei.rar@gmail.com>
2020-03-31 17:45:40 +0100

    update

--- run-ignore.sh
+++ run-ignore.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-node ./check-ignore.js
+yarn --silent exec nx print-affected -- --base=f5c4cfe --head=HEAD --select="projects"
+
+#yarn --silent exec 
+#f5c4cfe
+#${CACHED_COMMIT_REF}

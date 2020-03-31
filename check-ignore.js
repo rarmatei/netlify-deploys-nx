6b913daa62bb36f391f03ca218b83b6ddb9e050e
Rares Matei <matei.rar@gmail.com>
2020-03-31 14:32:01 +0100

    update

--- check-ignore.js
+++ check-ignore.js
@@ -4,6 +4,10 @@ const getAffected = `nx print-affected --base=${process.env.CACHED_COMMIT_REF} -
 exec(getAffected, function(error, stdout, stderr) {
   const changedProjects = JSON.parse(stdout).projects;
   const currentProject = process.env.PROJECT_NAME;
+  console.log('----CACHING-----');
+  console.log(changedProjects);
+  console.log(currentProject);
+  console.log(process.env.CACHED_COMMIT_REF);
   if (changedProjects.find(proj => proj === currentProject)) {
     process.exit(1);
   } else {

7bb96647ea78c506745161052ef40ce42303c1a9
Rares Matei <matei.rar@gmail.com>
2020-03-31 12:15:29 +0100

    update

--- apps/frontend-react/src/app/app.tsx
+++ apps/frontend-react/src/app/app.tsx
@@ -15,7 +15,7 @@ export const App = () => {
     <div className="app">
       <header className="flex">
         <Logo width="75" height="75" />
-        <h1>Welcome to frontend-react the NEW ONE2222!</h1>
+        <h1>Welcome to frontend-react the NEW ONE 33333</h1>
       </header>
       <main>
         <h2>Resources &amp; Tools</h2>

d1a6e7d1e6dc2378b2679d51fb34611a25f48c92
Rares Matei <matei.rar@gmail.com>
2020-04-03 15:19:01 +0100

    update

--- check-ignore.js
+++ check-ignore.js
@@ -1,19 +1,13 @@
 const exec = require('child_process').exec;
-const getAffected = `yarn --silent exec nx print-affected --base=${process.env.CACHED_COMMIT_REF} --head=HEAD`;
+const getAffected = `yarn --silent exec nx print-affected -- --base=${process.env.CACHED_COMMIT_REF} --head=HEAD`;
+const currentProject = process.env.PROJECT_NAME;
+
+if (!currentProject) {
+  process.exit(0);
+}
 
 exec(getAffected, function(error, stdout, stderr) {
-  process.stdout.write('hey there!');
-
-  if (error || stderr) {
-    process.stdout.write(error);
-    process.stdout.write(stderr);
-  }
   const changedProjects = JSON.parse(stdout).projects;
-  const currentProject = process.env.PROJECT_NAME;
-  console.log('----CACHING-----');
-  console.log(changedProjects);
-  console.log(currentProject);
-  console.log(process.env.CACHED_COMMIT_REF);
   if (changedProjects.find(proj => proj === currentProject)) {
     process.exit(0);
   } else {
--- run-ignore.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-yarn --silent exec nx print-affected -- --base=f5c4cfe --head=HEAD --select="projects"
-
-#yarn --silent exec 
-#f5c4cfe
-#${CACHED_COMMIT_REF}
